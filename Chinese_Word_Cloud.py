ab8677ac8072f6d8870579850dc6f60a55a13ddc
胡瑞祥 <45733030+HuRuixiang@users.noreply.github.com>
2019-05-05 17:48:10 +0800

    Update and rename word_cloud.py to Chinese_Word_Cloud.py

--- Chinese_Word_Cloud.py
+++ Chinese_Word_Cloud.py
@@ -1,27 +1,9 @@
-# #导入WordCloud库，可在命令行使用"pip install wordcloud"命令进行安装
-# from wordcloud import WordCloud
-
-# #读取文本
-# with open("yes-minister.txt","r",encoding='UTF-8') as f:
-#     mytext = f.read()
-# #生成词云
-# wcd = WordCloud(background_color="white",height=500,width=500)
-# wcd.generate(mytext)
-# #转换为照片格式
-# img = wcd.to_image()
-# #显示照片
-# img.show()
-# #存储照片
-# # img.save("wcd.png")
-
-##################################################################################
-#中文分词
 #导入WordCloud库，可在命令行使用"pip install wordcloud"命令进行安装
 from wordcloud import WordCloud
+#需导入jieba库，可在命令行使用"pip install jieba"命令进行安装
+import jieba
 import numpy as np
 from PIL import Image
-#需提前安装"jieba"库，可在命令行使用"pip install jieba"命令进行安装
-import jieba
 
 #读取文本
 with open("C:\\Users\\问津\\Desktop\\新建文本文档.txt","r") as f:
