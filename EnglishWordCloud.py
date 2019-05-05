ece5af7346f8f0fbddf30ece96a233bd1021c33b
胡瑞祥 <45733030+HuRuixiang@users.noreply.github.com>
2019-05-05 17:45:30 +0800

    Update and rename word_cloud.py to EnglishWordCloud.py

---
+++ EnglishWordCloud.py
@@ -0,0 +1,15 @@
+#导入WordCloud库，可在命令行使用"pip install wordcloud"命令进行安装
+from wordcloud import WordCloud
+
+#读取文本
+with open("yes-minister.txt","r",encoding='UTF-8') as f:
+    text = f.read()
+#生成词云
+wcd = WordCloud(background_color="white",height=500,width=500)
+wcd.generate(text)
+#转换为照片格式
+img = wcd.to_image()
+#显示照片
+img.show()
+#存储照片
+img.save("wcd.png")
